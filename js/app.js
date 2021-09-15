c3399395ff979e732e8ef800713afcc8b931ae61
trollering12312 <trollering12312@gmail.com>
2021-09-15 17:45:29 +0900

    Feat:지출관리 완성

--- js/app.js
+++ js/app.js
@@ -1,29 +1,18 @@
-//메인화면 : 리스트 추가
-jsonToHtml();
-
-//외부 json 파일을 가져옴
-function jsonToHtml() {
-	// `data`에 json 값들이 array형태로 저장
-	$.getJSON("https://syoon0624.github.io/json/test.json", function (data) {
-		const obj_list = [];
-		// json 처리
-		data["bankList"].forEach(function (item) {
-			//obj -> string
-			let json = JSON.stringify(item);
-			//string화된 obj를 기반으로 객체 생성
-			let parsed_obj = JSON.parse(json);
-			obj_list.push(parsed_obj);
-		});
-
-        const re_obj_list = obj_list.reverse();
-
-		const date_group_obj = date_group(re_obj_list);
-
-		//console.log(date_group_obj);
-
-		htmlMaker(date_group_obj);
+//json 처리 함수 json->obj_list
+function jsonProcess(data) {
+	const obj_list = [];
+	// json 처리
+	data["bankList"].forEach(function (item) {
+		//obj -> string
+		let json = JSON.stringify(item);
+		//string화된 obj를 기반으로 객체 생성
+		let parsed_obj = JSON.parse(json);
+		obj_list.push(parsed_obj);
 	});
+
+	return obj_list;
 }
+
 
 //객체 리스트를 날짜별로 묶은 객체 생성
 function date_group(obj_list) {
@@ -44,48 +33,56 @@ function date_group(obj_list) {
 	return date_group_obj;
 }
 
-function htmlMaker(date_group_obj) {
+//홈화면 html 요소 생성기
+function homeScreenGenerate(date_group_obj) {
 	let card_num = 0;
 
 	//초기화
 	let date = "";
-    /*let today_obj = Date.now();*/
-    let today_obj = new Date("2021-10-03");
+
+	/*let today_obj = Date.now();*/
+	//json데이터에서 임의로 마지막으로 설정
+	let today_obj = new Date("2021-10-03");
+
+	// for(let date_obj of Object.keys(date_group_obj)){
+	// 	for(let obj of Object.keys(date_group_obj[date_obj])){
+	// 		console.log(date_group_obj[date_obj][obj].classify);
+	// 	}
+	// }
 
 	Object.keys(date_group_obj).forEach((date_obj) => {
 		card_num++;
-        let total = 0;
+		let total = 0;
 
 		//expense_info
 		$(".expense").append(
 			$("<div/>")
-				.addClass("expense_info")
-				.attr("id", "card" + card_num.toString() + "-info")
+			.addClass("expense_info")
+			.attr("id", "card" + card_num.toString() + "-info")
 		);
 
 		//expense_list
 		$(".expense").append(
 			$("<ul/>")
-				.addClass("expense_list")
-				.attr("id", "card" + card_num.toString() + "-list")
+			.addClass("expense_list")
+			.attr("id", "card" + card_num.toString() + "-list")
 		);
 
+		//date 설정
+		date = date_obj;
 		Object.keys(date_group_obj[date_obj]).forEach((obj) => {
-			//date 설정
-			date = date_group_obj[date_obj][obj].date;
 
-            let date_ob = new Date(date);
-            
-            const dayToMS = 86400000;
+			let date_ob = new Date(date);
 
-            let diff = parseInt(((today_obj-date_ob)/dayToMS));
+			const dayToMS = 86400000;
 
-            if(diff == 0){
-                date = "오늘";
-            }
-            else if(diff>=1 && diff<=3){
-                date = diff + "일전"
-            }
+			let diff = parseInt(((today_obj - date_ob) / dayToMS));
+
+			if (diff == 0) {
+				date = "오늘";
+			} else if (diff >= 1 && diff <= 3) {
+				date = diff + "일전"
+			}
 
 			//total 계산, li추가
 			let inout = date_group_obj[date_obj][obj].income;
@@ -98,10 +95,8 @@ function htmlMaker(date_group_obj) {
 
 			let $span_price = $("<span/>", {
 				class: "price right",
-				text:
-					inout == "in"
-						? "+ " + date_group_obj[date_obj][obj].price
-						: date_group_obj[date_obj][obj].price
+				text: inout == "in" ?
+					"+ " + date_group_obj[date_obj][obj].price : date_group_obj[date_obj][obj].price
 			});
 
 			if (inout == "out") {
@@ -124,3 +119,186 @@ function htmlMaker(date_group_obj) {
 		);
 	});
 }
+
+//graph value calculation
+function expenseGenerate() {
+
+	$.getJSON("https://syoon0624.github.io/json/test.json", function (data) {
+		const obj_list = jsonProcess(data);
+		const date_group_obj = date_group(obj_list);
+
+		let monthly_category_dict = {
+			oiling: 0,
+			health: 0,
+			eatout: 0,
+			shopping: 0,
+			mart: 0
+		};
+		let monthly_expense_dict = {};
+
+		let total = 0;
+		let date = "";
+
+
+		//9월이라 가정함
+		//let today_obj = Date.now();
+		let today_obj = new Date("2021-09-01");
+		let month = today_obj.getMonth();
+
+		for (let date_obj of Object.keys(date_group_obj)) {
+
+			total = 0;
+
+			let date_obj_ = new Date(date_obj);
+
+			if (date_obj_.getMonth() != today_obj.getMonth()) {
+				break;
+			}
+
+			for (let obj of Object.keys(date_group_obj[date_obj])) {
+				//category dict 업데이트
+				let type = date_group_obj[date_obj][obj].classify;
+				if (date_group_obj[date_obj][obj].income == "out") {
+					monthly_category_dict[type] += parseInt(
+						date_group_obj[date_obj][obj].price
+					);
+
+					//daily total 계산
+					total += parseInt(date_group_obj[date_obj][obj].price);
+				}
+			}
+
+			//day dict 업데이트
+			monthly_expense_dict[date_obj_.getDate()] = total;
+		}
+
+		color_list = [
+			'rgb(255, 99, 132)',
+			'rgb(54, 162, 235)',
+			'rgb(255, 205, 86)',
+			'rgb(80, 205, 86)',
+			'rgb(255, 60, 86)'
+		];
+
+		monthly_expense_chart_maker(monthly_expense_dict);
+
+		//월 설정하기
+		$(".month_pattern > h2").text((month + 1) + " 월 지출 패턴");
+		monthly_category_maker(monthly_category_dict, color_list)
+
+		setCategoryTotal(monthly_category_dict, color_list);
+	})
+}
+
+
+//daily_chart-bar
+function monthly_expense_chart_maker(monthly_expense_dict) {
+	const daily_chart = $('.daily');
+
+	const labels = Object.keys(monthly_expense_dict);
+	const data_ = Object.values(monthly_expense_dict);
+
+	const data = {
+		labels: Object.keys(monthly_expense_dict),
+		datasets: [{
+			type: 'bar',
+			label: '일별 지출',
+			data: Object.values(monthly_expense_dict),
+			backgroundColor: "#bfff00",
+			barPercentage:0.5
+		},{
+			type: 'line',
+			label: '일별 지출',
+			data: Object.values(monthly_expense_dict),
+			borderColor: "#fff457",
+			fill: false,
+			borderDash : [3]
+		}
+	],
+	};
+
+	const config = {
+		type: 'scatter',
+		data: data,
+		options: {
+		  scales: {
+			y: {
+			  beginAtZero: true
+			}
+		  }
+		}
+	  };
+
+	// const daily_Chart = new Chart(daily_chart, config);
+	var mixedChart = new Chart(daily_chart, config);
+}
+
+
+
+//month_chart-doughnut
+
+function monthly_category_maker(monthly_category_dict, color_list) {
+	const month_chart = $('.month');
+
+	const labels = Object.keys(monthly_category_dict);
+	const data = Object.values(monthly_category_dict);
+
+	const month_data = {
+		labels: labels,
+		datasets: [{
+			label: 'My First Dataset',
+			data: data,
+			backgroundColor: color_list,
+			hoverOffset: 4,
+			radius : 200
+		}]
+	};
+
+	const month_config = {
+		type: 'doughnut',
+		data: month_data,
+	};
+
+	const month_Chart = new Chart(month_chart, month_config);
+
+}
+
+//카테고리 설정
+function setCategoryTotal(monthly_category_dict, color_list) {
+
+	$(".category_list .category_price").each(function (i, item) {
+
+		let totals = Object.values(monthly_category_dict);
+		$(item).text(parseInt(totals[i]) + "원");
+	})
+
+	$(".management .ico").each(function (i, item) {
+		$(item).css('color',color_list[i]);
+	})
+
+}
+
+//외부 json 파일을 가져옴
+function homeGenerate() {
+
+	// `data`에 json 값들이 array형태로 저장
+	$.getJSON("https://syoon0624.github.io/json/test.json", function (data) {
+
+		const obj_list = jsonProcess(data);
+
+		//역순으로 변환
+		const re_obj_list = obj_list.reverse();
+		const re_date_group_obj = date_group(re_obj_list);
+
+		//홈화면
+		homeScreenGenerate(re_date_group_obj);
+	})
+
+}
+
+
+//홈 화면
+homeGenerate()
+
+//지출관리
+expenseGenerate();
